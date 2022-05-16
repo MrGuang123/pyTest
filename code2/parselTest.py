8d46bc41bec8aeed78917b1488e874a507382378
ytg <522125842@qq.com>
2022-05-16 22:19:03 +0800

    parsel test

---
+++ code2/parselTest.py
@@ -0,0 +1,26 @@
+from parsel import Selector
+
+
+html = '''
+  <div>
+    <ul>
+      <li class="item-0">first item</li>
+      <li class="item-1"><a href="link2.html">second item</a></li>
+      <li class="item-inactive"><a href="link3.html"><span class="bold">third item</span></a></li>
+      <li class="item-1"><a href="link4.html">fourth item</a></li>
+      <li class="item-0"><a href="link5.html">fifth item</a>
+    </ul>
+  </div>
+'''
+
+selector = Selector(text=html)
+
+# items = selector.css('.item-0')
+# print(len(items), type(items), items)
+# items2 = selector.xpath('//li[contains(@clsss, "item-0")]')
+# print(len(items), type(items), items)
+
+items = selector.css('.item-0')
+for item in items:
+    text = item.xpath('.//text()').get()
+    print(text)
